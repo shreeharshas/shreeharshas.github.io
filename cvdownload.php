9bc7e4f27f7c17eacb931f613ac77efdef03aa06
Shree Harsha Sridharamurthy <sridhash@iu.edu>
2016-12-31 01:55:37 -0500

    init

---
+++ cvdownload.php
@@ -0,0 +1,28 @@
+header("Content-Type: application/octet-stream");
+
+$file = $_GET["file"] .".pdf";
+header("Content-Disposition: attachment; filename=" . urlencode($file));   
+header("Content-Type: application/octet-stream");
+header("Content-Type: application/download");
+header("Content-Description: File Transfer");            
+header("Content-Length: " . filesize($file));
+flush();
+$fp = fopen($file, "r");
+while (!feof($fp))
+{
+    echo fread($fp, 65536);
+    flush(); // this is essential for large downloads
+} 
+fclose($fp); 
+
+
+		<script>
+			  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+				  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+	
+			  ga('create', 'UA-83912099-1', 'auto');
+			  ga('send', 'pageview');
+
+		</script>
