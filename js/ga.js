fe54d8046e584f1dbf8409c0d3b744cb89bd9141
Shree Harsha Sridharamurthy <shreeharshas@users.noreply.github.com>
2017-01-07 19:27:45 -0500

    Update ga.js

--- js/ga.js
+++ js/ga.js
@@ -1,7 +1,8 @@
 $(window).on('load', function() {
     // Just replacing the value of the 'content' attribute will not work.
     $('head').append('<meta name="viewport" content="width=device-width, initial-scale=1.0">');
-	console.log("done");
+	var x = document.getElementsByTagName("head");
+	console.log(x);
 });
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
